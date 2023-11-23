d600b99f986ec4be7efd098b742b300ca6f4e063
unknown <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>
2023-11-23 16:10:27 +0800

    test 123

--- utils/settings.ts
+++
@@ -1,9 +0,0 @@
-import { authHeaders, baseUrl } from "./api.config";
-
-export async function getSettings(token: any) {
-  const res = await fetch(`${baseUrl}/api/setting`, {
-    headers: {...authHeaders(token)}
-  });
-  const json = await res.json();
-  return json;
-}
